3bfa4cf0620099ea9dd4c73d307fa145b5e0c4b0
shoei_sato <46258448+satoss@users.noreply.github.com>
2021-02-03 19:29:26 +0900

    learn useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>This is a template</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const increment2 = () => {
+    setCount((previousCount) => previousCount + 1);
+  };
+
+  const decrement2 = () => {
+    setCount((previousCount) => previousCount - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  const multipliedBy2 = () => {
+    setCount(count * 2);
+  };
+
+  const dividedBy3 = () => {
+    if (count % 3 === 0) {
+      setCount(count / 3);
+    }
+  };
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={resetCount}>Reset</button>
+        <button onClick={multipliedBy2}>x2</button>
+        <button onClick={dividedBy3}>３の倍数の時だけ３で割る</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
