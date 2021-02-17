826af5540fa2261cd523e3364cc463b27401194a
shoei_sato <46258448+satoss@users.noreply.github.com>
2021-02-17 14:10:34 +0900

    update operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,7 +1,13 @@
 import React, { useState, useContext } from "react";
 
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from "../actions";
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS,
+} from "../actions";
 import AppContext from "../contexts/AppContext";
+import { timeCurrentIso8601 } from "../utils";
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -15,6 +21,11 @@ const EventForm = () => {
       title,
       body,
     });
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: "イベントを作成しました。",
+      operatedAt: timeCurrentIso8601(),
+    });
     setTitle("");
     setBody("");
   };
@@ -27,6 +38,11 @@ const EventForm = () => {
     if (result) {
       dispatch({
         type: DELETE_ALL_EVENTS,
+      });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: "全てのイベントを削除しました。",
+        operatedAt: timeCurrentIso8601(),
       });
     }
   };
