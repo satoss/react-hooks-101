9684277307d4e3a9fbbf7256dc60d3068740940c
shoei_sato <46258448+satoss@users.noreply.github.com>
2021-02-17 14:34:02 +0900

    create operaion log case of event deletion

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 
-import { DELETE_EVENT } from "../actions";
+import { ADD_OPERATION_LOG, DELETE_EVENT } from "../actions";
+import { timeCurrentIso8601 } from "../utils";
+
 import AppContext from "../contexts/AppContext";
 
 const Event = ({ event }) => {
@@ -14,6 +16,11 @@ const Event = ({ event }) => {
       dispatch({
         type: DELETE_EVENT,
         id,
+      });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: `イベント(id=${id})を削除しました。`,
+        oparatedAt: timeCurrentIso8601(),
       });
     }
   };
